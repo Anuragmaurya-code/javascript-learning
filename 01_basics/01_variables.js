03bdf9165c92ebc9e30de2ecfe6087e9ec9c13d3
Anurag Maurya <76918434+Anuragmaurya-code@users.noreply.github.com>
2023-11-27 13:00:14 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=14484
+let accountEmail="anuragmaurya@gmail.com"
+var accountPassword='1234'
+accountCity="Mumbai"
+let accountState;
+// accountId = 2 // not allowed
+/*
+use let
+ */
+accountEmail="ajdjdkfj@gmail.com"
+accountPassword="83838833"
+accountCity="UP"
+console.log(accountCity)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState ])
---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all js as a new version
+
+// alert(3+3); // alert wont run in node and not browser
+// console.log("chup"); console.log("chap")
+
