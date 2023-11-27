8a780cdd755e536b23c1324d7a725e572033d760
Anurag Maurya <mauryaanurag375@gmail.com>
2023-11-27 18:40:22 +0530

    commit

--- 01_basics/02_data_types.js
+++ 01_basics/02_data_types.js
@@ -1,5 +1,20 @@
 "use strict"; // treat all js as a new version
 
+
+let age=9
 // alert(3+3); // alert wont run in node and not browser
 // console.log("chup"); console.log("chap")
 
+
+// premitive data type (data types from which all the data types are derived)
+// number => range is 2 to power 53
+// bigint for very big 
+// string => ""
+//  boolean => ""
+// null => standalone value (khali)
+// undefined => not assigned any value
+// symbol => unique
+
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
